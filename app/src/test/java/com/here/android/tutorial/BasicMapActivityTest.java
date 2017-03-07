d94cbb484add1cff23b87aae52a58b9ad014422c
Emina Dzuzdanovic <emina.dzuzdanovic@bstelecom.ba>
2017-03-07 08:47:18 +0100

    Initial commit

---
+++ app/src/test/java/com/here/android/tutorial/BasicMapActivityTest.java
@@ -0,0 +1,14 @@
+package com.here.android.tutorial;
+
+import org.junit.Test;
+
+/**
+ * Created by emina.dzuzdanovic on 06/03/2017.
+ */
+public class BasicMapActivityTest {
+    @Test
+    public void onCreate() throws Exception {
+
+    }
+
+}
